7b0f3b47efac60a662d01e5664558e79df330588
luxkyluke <rock_on77500@hotmail.fr>
2016-10-17 21:00:18 +0200

    eigen update

---
+++ field.cpp
@@ -0,0 +1,6 @@
+#include "field.h"
+
+
+Field::Field(int nb_row, int nb_col){
+    matrix
+}
---
+++ field.h
@@ -0,0 +1,15 @@
+#ifndef FIELD_H
+#define FIELD_H
+#include <eigen/Eigen/Dense>
+
+using namespace Eigen;
+
+class Field
+{
+    MatrixXi matrix;
+
+public:
+    Field();
+};
+
+#endif // FIELD_H
