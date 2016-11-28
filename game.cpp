5c06a6c5469301763032a6ff5545de6ef0e58b2d
luxkyluke <rock_on77500@hotmail.fr>
2016-11-28 18:01:55 +0100

    oublie

--- game.cpp
+++ game.cpp
@@ -13,17 +13,21 @@ const static int FRAME_DURATION = 17;
 
 //}
 
-Game::Game(const int w, const int h, Player* p1): terrain(w, h),
-     window(this){
+Game::Game(const int w, const int h, Player* p1) :
+        terrain(this, w, h){
 
-    window.installEventFilter(this);
+    MainWindow* window = new MainWindow(w, h);
+    window->installEventFilter(this);
+
+    //terrain.setParent(window);
+    window->setCanvas(&terrain);
 
     QTimer* timer = new QTimer(this);
     QObject::connect(timer, SIGNAL(timeout()), this, SLOT(refresh()));
     timer->start(FRAME_DURATION);
 
     players.append(p1);
-    window.show();
+    window->show();
 }
 
 void Game::addPlayer(Player *p){
@@ -42,6 +46,10 @@ bool Game::keyEvent(QKeyEvent* event){
         p->checkKey(event);
     }
     return true;
+}
+
+Terrain* Game::getTerrain(){
+    return &terrain;
 }
 
 void Game::updateScene(){
@@ -69,7 +77,7 @@ int Game::getHeight() const{
 
 void Game::refresh(){
     updateScene();
-    window.repaintRenderer();
+    //window.repaintRenderer();
     //terrain.update(window.getRenderer());
 }
 
