cb41d64d665753542ec069d17778dd74e7f2c995
luxkyluke <rock_on77500@hotmail.fr>
2016-12-11 14:25:42 +0100

    Formulaire pas trop mal

--- menu.cpp
+++ menu.cpp
@@ -3,24 +3,77 @@
 
 #include <QString>
 #include <iostream>
+#include <QKeyEvent>
+#include <QDebug>
+
+static int ID_INCREMENTATOR=0;
 
 MenuWindow::MenuWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MenuWindow){
     ui->setupUi(this);
-    ui->pseudoTextEdit->setText("Player1");
-    QObject::connect(ui->pseudoTextEdit, SIGNAL(textChanged()),
-                     this, SLOT(checkTextEditWidth()));
+
+    addPlayer();
+
+    initWindow();
+
+
+
+
 }
+
+void MenuWindow::initWindow(){
+    ui->WidgetPlayer_1->setVisible(false);
+
+//    ui->pseudoTextEdit->setText("Player1");
+//    QObject::connect(ui->pseudoTextEdit, SIGNAL(textChanged()),
+//                     this, SLOT(checkTextEditWidth()));
+//    ui->pseudoTextEdit->installEventFilter(this);
+//    foreach(player* p, players){
+
+//    }
+}
+
+void MenuWindow::addPlayer(){
+    player p ;
+    p.id = ID_INCREMENTATOR++;
+    p.leftKey = Qt::Key_Left;
+    p.rightKey = Qt::Key_Right;
+    p.pseudo = std::string("Player_") + std::to_string(ID_INCREMENTATOR);
+    players.append(&p);
+}
+
+bool MenuWindow::eventFilter(QObject *object, QEvent *event) {
+    if (object == ui->pseudoTextEdit_8 && event->type() == QEvent::KeyPress){
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+
+        if (keyEvent->key() == Qt::Key_A)	{
+            qDebug("Enter Key Pressed...");
+            qDebug()<<keyEvent->key();
+            return true;
+        }
+    }
+    return QWidget::eventFilter(object,event);
+}
+
 
 MenuWindow::~MenuWindow(){
     delete ui;
 }
 
 void MenuWindow::checkTextEditWidth(){
-    QString text = ui->pseudoTextEdit->toPlainText();
+    QString text = ui->pseudoTextEdit_2->toPlainText();
     if(text.length() > 15){
-        ui->pseudoTextEdit->setText(text.left(15));
+        ui->pseudoTextEdit_2->setText(text.left(15));
     }
+}
+
+void MenuWindow::on_checkBox_clicked(bool checked){
+    ui->WidgetPlayer_1->setVisible(true);
+}
+
+
+void MenuWindow::on_WidgetPlayer_0_objectNameChanged(const QString &objectName)
+{
 
 }
--- menu.h
+++ menu.h
@@ -2,14 +2,33 @@
 #define MENU_H
 
 #include <QMainWindow>
+#include <QEvent>
+#include <QVector>
+#include <stdlib.h>
 
 namespace Ui {
 class MenuWindow;
 }
+
+struct player{
+    int id;
+    int rightKey;
+    int leftKey;
+    std::string pseudo;
+};
+
 class MenuWindow : public QMainWindow{
     Q_OBJECT
+    QVector<player*>  players;
+
+    void addPlayer();
+    void initWindow();
+
 public:
     explicit MenuWindow(QWidget *parent=0);
+
+    bool eventFilter(QObject *object, QEvent *event);
+
     ~MenuWindow();
 
 private:
@@ -18,6 +37,9 @@ private:
 public slots:
     void checkTextEditWidth();
 
+private slots:
+    void on_checkBox_clicked(bool checked);
+    void on_WidgetPlayer_0_objectNameChanged(const QString &objectName);
 };
 
 
